184cdc2d6f5e14837060ec3426fe5b101509a165
SaiyaraIslam <sislam24@cougarnet.uh.edu>
2025-02-15 18:38:37 -0600

    second commit

--- create_vault.py
+++ create_vault.py
@@ -1,12 +1,12 @@
-import mysql.connector
 import creds
-from mysql.connector import Error
 from sql import create_connection
 from sql import execute_query
-from sql import execute_read_query
+
 
 #creating db connection
 myCreds = creds.Creds()
 conn = create_connection(myCreds.conString,myCreds.userName, myCreds.password, myCreds.dbName)
 
 query = "INSERT INTO vault (VaultNum, VaultCode, Content, Owner) VALUES (%s, %s, %s, %s)"
+values = (100, 12345, "This is secret content from vault 1", "AI Capone")
+execute_query(conn, query, values)
