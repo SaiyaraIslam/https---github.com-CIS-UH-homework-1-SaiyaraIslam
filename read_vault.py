7f2c1a72680c74e89147ce7d46f8b45dd1907ce9
SaiyaraIslam <sislam24@cougarnet.uh.edu>
2025-02-15 19:48:42 -0600

    final commit

--- read_vault.py
+++ read_vault.py
@@ -1,1 +1,14 @@
 import mysql.connector
+from sql import create_connection
+from sql import execute_query
+import creds
+# importing all the connections
+
+#connecting to mysql
+myCreds = creds.Creds()
+conn = conn = create_connection(myCreds.host, myCreds.user, myCreds.password, myCreds.db_name)
+# reading the vault content
+def read_vault():
+    query = "INSERT INTO Content, Owner FRON vault (VaultNum, VaultCode) VALUES (%s, %s)"
+    cursor = conn.cursor()
+    execute_query(conn, query) #executing the results
